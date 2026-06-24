17523fd298c12ab80ddec966804be66723b1fbdf
DiegoAaronS <123047151@upq.edu.mx>
2026-06-23 22:42:10 -0600

    Practica No.11: Pressable & Switch

--- practica5/miapp2/screens/PressableScreen.js
+++ practica5/miapp2/screens/PressableScreen.js
@@ -1,26 +1,92 @@
-/* Zona1: Importaciones de componentes y archivos */
+// Zona 1: Importaciones de componentes y archivos
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, View, StyleSheet, Text, Switch } from "react-native";
+import { useState } from "react";
 
-/* Zona2: Main - Hogar de los componentes */
-export default function SafeAreaScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Aquí el screen de Pressable</Text>
-      <StatusBar style="auto" />
+// Zona 2: Main - Hogar de los componentes
+export default function PressableScreen() {
+    const [buttonText, setButtonText] = useState('Dame Clic');
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
-    </View>
-  );
+    return (
+        <View style={[
+            styles.container, 
+            { backgroundColor: isDarkMode ? "#000000" : "#ffffff" }
+        ]}>
+            
+            {/* Ajusta el color de la barra de estado del celular según el modo */}
+            <StatusBar style={isDarkMode ? "light" : "dark"} />
+
+            {/* SECCIÓN 1: Botón Pressable */}
+            <Pressable 
+                style={styles.button}
+                onPress={() => {
+                    console.log("Se presionó el botón");
+                    setButtonText("Botón presionado");
+                }}
+                onPressIn={() => {
+                    console.log("Se acaba de presionar el botón");
+                }}
+                onPressOut={() => {
+                    console.log("Se acaba de soltar el botón");
+                }}
+                onLongPress={() => {
+                    console.log("Presión prolongada");
+                    setButtonText("Presión prolongada detectada");
+                }}
+            >
+                <Text style={styles.buttonText}>{buttonText}</Text>
+            </Pressable>
+
+            {/* SECCIÓN 2: Switch del Modo Oscuro */}
+            <View style={styles.switchContainer}>
+                <Text style={[
+                    styles.text, 
+                    { color: isDarkMode ? "#ffffff" : "#000000" }
+                ]}>
+                    Dark Mode
+                </Text>
+
+                <Switch
+                    value={isDarkMode}
+                    onValueChange={() => setIsDarkMode(!isDarkMode)}
+                    trackColor={{false: "#767577", true: "lightblue"}}
+                    thumbColor="#f4f3f4"
+                />
+            </View>
+
+        </View>
+    );
 }
 
-/* Zona3: Estilos y posicionamiento */
+// Zona 3: Estilos - Personalización de los componentes
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    flexDirection: 'row-reverse',
-    paddingHorizontal: 20,
-  },
+    container: {
+        flex: 1, 
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
+    },
+    button: {
+        backgroundColor: "blue",
+        padding: 20,
+        borderRadius: 10,
+        marginBottom: 50 
+    },
+    buttonText: {
+        fontSize: 20,
+        color: "white",
+        textAlign: "center"
+    },
+    switchContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "80%", 
+        paddingHorizontal: 10
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: "bold"
+    }
 });
