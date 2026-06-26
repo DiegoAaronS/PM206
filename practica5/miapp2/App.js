7fc972eb5e348eac75693e5f2873d32396bac033
DiegoAaronS <123047151@upq.edu.mx>
2026-06-25 23:32:10 -0600

    Practica No.12: TextInput & AlertPractica No.12: TextInput & Alert

--- practica5/miapp2/App.js
+++ practica5/miapp2/App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffbc58',
+    backgroundColor: '#1f1f1f',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'column',
